7a4bc348c14aa01f3b99978c5b0486d342143b6b
Dawid Adamus <dawidadamus@MacBook-Pro-Dawid.local>
2017-08-15 23:35:41 +0200

    nie wiem co jest zjebane

--- engine/costing/plateMultiPart/view/directory/directoryContentView.php
+++ engine/costing/plateMultiPart/view/directory/directoryContentView.php
@@ -1,15 +1,13 @@
 <?php
 $dir_count = 0;
-?>
-
-<?php foreach($data as $dir): ?>
+foreach ($data as $dir): ?>
     <tr data-id="<?= $dir["id"] ?>" class="multiDirChoose">
         <td style="text-align: center; cursor: pointer;"><?= $dir["name"] ?></td>
     </tr>
     <?php
-        $dir_count++;
-        if ($dir_count > 5) {
-            break;
-        }
+    $dir_count++;
+    if ($dir_count > 5) {
+        break;
+    }
     ?>
 <?php endforeach; ?>
--- sql/0.2/post_update.sql
+++ sql/0.2/post_update.sql
@@ -1,71 +1,77 @@
+DROP TABLE plate_multiPartDirectories;
+DROP TABLE plate_multiPartDetails;
+DROP TABLE plate_multiPartCostingDetails;
+DROP TABLE plate_multiPartPrograms;
+DROP TABLE plate_multiPartProgramsPart;
+DROP TABLE plate_multiPartCostingMaterial;
+
 CREATE TABLE IF NOT EXISTS `plate_multiPartDirectories` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `dir_name` varchar(32) NOT NULL,
-  `created_at` datetime NOT NULL,
+  `dir_name` varchar(32),
+  `created_at` datetime,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin2 AUTO_INCREMENT=1,
+) ENGINE=InnoDB DEFAULT CHARSET=latin2 AUTO_INCREMENT=1;
 
 CREATE TABLE IF NOT EXISTS `plate_multiPartDetails` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `mpw` int(11) NOT NULL,
-  `did` int(11) NOT NULL,
-  `src` varchar(32) NOT NULL,
+  `name` VARCHAR(32),
+  `mpw` int(11),
+  `did` int(11),
+  `src` varchar(32),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 CREATE TABLE IF NOT EXISTS `plate_multiPartCostingDetails` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `did` int(11) NOT NULL,
-  `LaserMatName` varchar(64) NOT NULL,
-  `PreTime` varchar(16) NOT NULL,
-  `upload_date` datetime NOT NULL,
+  `did` int(11),
+  `LaserMatName` varchar(64),
+  `PreTime` varchar(16),
+  `upload_date` datetime,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 CREATE TABLE IF NOT EXISTS `plate_multiPartPrograms` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `SheetName` varchar(64) NOT NULL,
-  `materialId` int(11) NOT NULL,
-  `UsedSheetNum` int(11) NOT NULL,
-  `CreateDate` datetime NOT NULL,
+  `SheetName` varchar(64),
+  `materialId` int(11),
+  `UsedSheetNum` int(11)  ,
+  `CreateDate` datetime  ,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 CREATE TABLE IF NOT EXISTS `plate_multiPartProgramsPart` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `PartNo` int(11) NOT NULL,
-  `PartName` varchar(64) NOT NULL,
-  `PartCount` float NOT NULL,
-  `UnfoldXSize` float NOT NULL,
-  `UnfoldYSize` float NOT NULL,
-  `RectangleArea` float NOT NULL,
-  `RectangleAreaW` float NOT NULL,
-  `RectangleAreaWO` float NOT NULL,
-  `Weight` float NOT NULL,
-  `LaserMatName` varchar(64) NOT NULL,
-  `ProgramId` int(11) NOT NULL,
-  `CreateDate` datetime NOT NULL,
+  `DetailId` INT(11) ,
+  `PartNo` int(11)  ,
+  `PartName` varchar(64)  ,
+  `PartCount` float  ,
+  `UnfoldXSize` float  ,
+  `UnfoldYSize` float  ,
+  `RectangleArea` float  ,
+  `RectangleAreaW` float  ,
+  `RectangleAreaWO` float  ,
+  `Weight` float  ,
+  `LaserMatName` varchar(64)  ,
+  `ProgramId` int(11)  ,
+  `CreateDate` datetime  ,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 CREATE TABLE IF NOT EXISTS `plate_multiPartCostingMaterial` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `SheetCode` varchar(65) NOT NULL,
-  `UsedSheetNum` int(11) NOT NULL,
-  `MatName` varchar(32) NOT NULL,
-  `thickness` float NOT NULL,
-  `SheetSize` varchar(32) NOT NULL,
-  `density` float NOT NULL,
-  `price` float NOT NULL,
-  `prgSheetPrice` float NOT NULL,
-  `created_ad` datetime NOT NULL,
+  `SheetCode` varchar(65)  ,
+  `UsedSheetNum` int(11)  ,
+  `MatName` varchar(32)  ,
+  `thickness` float  ,
+  `SheetSize` varchar(32)  ,
+  `density` float  ,
+  `price` float  ,
+  `prgSheetPrice` float  ,
+  `created_ad` datetime  ,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
-ALTER TABLE `plate_costingFrame` CHANGE type type ENUM('singePartCosting', 'multiPartCosting'),
-ALTER TABLE `plate_costingFrame` ADD `programId` INT(11) NOT NULL,
-ALTER TABLE `plate_singlePartCosting_image` RENAME `plate_CostingImage`,
-ALTER TABLE `plate_CostingImage` CHANGE `plate_costingType` `plate_costingType` ENUM('singePartCosting', 'multiPartCosting'),
-
-ALTER TABLE `plate_multiPartProgramsPart` ADD  `DetailId` INT NOT NULL AFTER  `id`;
-ALTER TABLE `plate_multiPartDetails` ADD `name` VARCHAR(32);
+ALTER TABLE `plate_costingFrame` CHANGE type type ENUM('singePartCosting', 'multiPartCosting');
+ALTER TABLE `plate_costingFrame` ADD `programId` INT(11)  ;
+ALTER TABLE `plate_singlePartCosting_image` RENAME `plate_CostingImage`;
+ALTER TABLE `plate_CostingImage` CHANGE `plate_costingType` `plate_costingType` ENUM('singePartCosting', 'multiPartCosting');
