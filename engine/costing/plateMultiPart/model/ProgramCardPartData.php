1ca60a13e2df506c6622df1437d30f787bdc25a8
Dawid Adamus <dawidadamus@MacBook-Pro-Dawid.local>
2017-08-28 21:27:21 +0200

    literowka

--- engine/costing/plateMultiPart/model/ProgramCardPartData.php
+++ engine/costing/plateMultiPart/model/ProgramCardPartData.php
@@ -174,8 +174,6 @@ class ProgramCardPartData
             }
         }
 
-        echo "1";
-
         $checkQuery = $db->prepare("SELECT id FROM plate_multiPartProgramsPart WHERE PartName = :partName AND ProgramId = :programId");
         $checkQuery->bindValue(":partName", $this->getPartName(), PDO::PARAM_STR);
         $checkQuery->bindValue(":programId", $programId, PDO::PARAM_INT);
@@ -184,22 +182,18 @@ class ProgramCardPartData
 
         $saveQuery = new sqlBuilder(sqlBuilder::INSERT, "plate_multiPartProgramsPart");
         if ($checkQueryResult !== false) {
-            echo "2";
             $saveQuery = new sqlBuilder(sqlBuilder::UPDATE, "plate_multiPartProgramsPart");
             $saveQuery->addCondition("PartName = '" . $this->getPartName() . "' AND ProgramId = " . $programId);
         } else {
             //Trzeba zapisac obrazek detalu
             $this->saveDetailImg();
-            echo "3";
         }
 
         $partId = $this->getId();
         if ($partId > 0) {
-            echo "4";
             $saveQuery = new sqlBuilder(sqlBuilder::UPDATE, "plate_multiPartProgramsPart");
             $saveQuery->addCondition("id = " . $partId);
         }
-        echo "5";
 
         $saveQuery->bindValue("DetailId", $this->getDetailId(), PDO::PARAM_INT);
         $saveQuery->bindValue("PartNo", $this->getPartNo(), PDO::PARAM_INT);
@@ -213,6 +207,7 @@ class ProgramCardPartData
         $saveQuery->bindValue("Weight", $this->getWeight(), PDO::PARAM_STR);
         $saveQuery->bindValue("LaserMatName", $this->getLaserMatName(), PDO::PARAM_STR);
         $saveQuery->bindValue("ProgramId", $programId, PDO::PARAM_INT);
+        $saveQuery->flush();
     }
 
     private function saveDetailImg()
